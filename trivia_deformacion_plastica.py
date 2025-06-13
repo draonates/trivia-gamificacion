c9eb91f73e4393b5de709b37e8d9383276a41c1c
draonates <giovanni.onate@gmail.com>
2025-06-12 21:56:11 -0400

    Update trivia_deformacion_plastica.py

--- trivia_deformacion_plastica.py
+++ trivia_deformacion_plastica.py
@@ -1,3 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Trivia Deformación Plástica",
+    layout="centered"
+)
+
+# Luego puedes importar el resto
+import pandas as pd
+import datetime
+import os
+import matplotlib.pyplot as plt
+import math
+
 import gspread
 import base64
 import requests
